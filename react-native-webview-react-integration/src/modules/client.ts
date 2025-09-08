d69e376f3ddb62a1616fb6876bfc14f69aaa07ba
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-09-08 11:08:48 +0900

    move folder

--- react-native-webview-react-integration/src/modules/client.ts
+++
@@ -1,8 +0,0 @@
-import HackleManager from "hackle-js-bridge";
-
-const manager = new HackleManager();
-const hackleClient = manager.createInstance(import.meta.env.VITE_SDK_KEY, {
-  exposureEventDedupIntervalMillis: 1000,
-});
-
-export default hackleClient;
